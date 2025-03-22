1286ddc30fa4ed4fadb0960c4388059e57445a59
Cybrarist <38886749+Cybrarist@users.noreply.github.com>
2025-03-22 07:53:26 +0400

    - add notification to be sent when container is up.
    Took 2 minutes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -50,7 +50,6 @@ php artisan optimize
 
 php artisan make:filament-user --name=$DEFAULT_USER --email=$DEFAULT_EMAIL --password=$DEFAULT_PASSWORD
 
-
 php artisan octane:install --server=frankenphp
 
 Xvfb :99 -screen 0 2000x2000x24 & export DISPLAY=:99
