828067781559ebcc4d431b2cd7d2050719a4815b
Cybrarist <38886749+Cybrarist@users.noreply.github.com>
2025-11-06 20:32:27 +0400

    clear old project

--- resources/views/vendor/filament-breezy/filament/pages/my-profile.blade.php
+++
@@ -1,9 +0,0 @@
-<x-filament::page>
-    <div class="space-y-6 divide-y divide-gray-900/10 dark:divide-white/10">
-        @foreach ($this->getRegisteredMyProfileComponents() as $component)
-            @unless(is_null($component))
-                @livewire($component)
-            @endunless
-        @endforeach
-    </div>
-</x-filament::page>
